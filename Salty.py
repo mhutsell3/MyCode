45335148630f5f9401ab06f261957d3ccdc31cf1
Milford Hutsell <Milford Hutsell.gmail.com>
2021-04-27 14:28:00 +0000

    mixed Lists

---
+++ Salty.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+icecream=["Flavors","Salty"]
+icecream.append(99)
+print("What is your first name")
+name=input()
+print(icecream[2],"" + icecream[0] +", and " + name +" chooses to be", icecream[1],".")
+
+
+
---
+++ dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+icecream=["Flavors","Salty"]
+icecream.append(99)
+heroes=  {
+"wolverine":
+    {"real name": "James Howlett",
+    "powers": "regeneration",
+    "archenemy": "Sabertooth",},
+"harry potter":
+    {"real name": "Harry Potter",
+    "powers": "magic",
+    "archenemy": "Voldemort",},
+"captain america":
+    {"real name": "Steve Rogers",
+    "powers": "frisbee shield",
+    "archenemy": "Hydra",}
+        }
+print("Which character do you want to know about?", heroes.keys())
+name=input()
+print("Which statistic would you like to know about?, input.keys())
+attribute=input()
+
